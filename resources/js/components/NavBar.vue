b5e70f9d7ed17b80f54eb65a1c5e2389fd0dee30
Liwa CHERIF <l.cherif@vaerdia.com>
2025-12-25 16:28:39 +0100

    Fix: Proper mobile menu scroll with flexbox layout

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -170,7 +170,7 @@ async function handleLogout() {
             <Transition name="slide">
                 <div 
                     v-if="isMobileMenuOpen"
-                    class="fixed top-0 right-0 h-full w-72 bg-white shadow-xl z-50 lg:hidden overflow-y-auto"
+                    class="fixed top-0 right-0 h-full w-72 bg-white shadow-xl z-50 lg:hidden flex flex-col"
                 >
                     <!-- Sidebar Header -->
                     <div class="flex items-center justify-between p-4 border-b border-gray-200">
@@ -199,7 +199,7 @@ async function handleLogout() {
                     </div>
 
                     <!-- Navigation Links -->
-                    <div class="py-4 pb-24">
+                    <div class="py-4 flex-1 overflow-y-auto">
                         <div class="px-4 py-2 text-xs font-semibold text-gray-400 uppercase tracking-wider">
                             Navigation
                         </div>
@@ -334,7 +334,7 @@ async function handleLogout() {
                     </div>
 
                     <!-- Bottom Actions -->
-                    <div class="absolute bottom-0 left-0 right-0 p-4 border-t border-gray-200 bg-white">
+                    <div class="flex-shrink-0 p-4 border-t border-gray-200 bg-white">
                         <template v-if="authStore.user || authStore.isAdmin">
                             <button 
                                 @click="handleLogout"
